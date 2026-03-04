a9f71f83fa96be52eb733160fd28d8b073155a4e
Vitor Moura <jvrezendemoura@gmail.com>
2026-03-04 20:44:23 -0300

    Updating the uploader component

--- frontend/src/components/ImageUploader.vue
+++ frontend/src/components/ImageUploader.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { ref } from "vue";
 
+const selectedFile = ref<File | null>(null);
 
+function onFileChange(e: Event) {
+  const target = e.target as HTMLInputElement
+  selectedFile.value = target.files?.[0] ?? null
+}
+
+defineExpose({ selectedFile });
 </script>
 <template>
-    <input type="file" class="file-input">
+  <input type="file" class="file-input" @change="onFileChange" />
 </template>
 <style>
 
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import Header from './components/Header.vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+app.component(
+  "Header", Header 
+);
+
 
 app.use(createPinia())
 app.use(router)
